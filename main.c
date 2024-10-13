3cd4cbceae9c43e3b285e10a85b48a896f96e918
Aditya Ranjan Barik <aditya.barik32@gmail.com>
2024-10-13 17:29:43 +0530

    disk persistence added

--- main.c
+++ main.c
@@ -199,7 +199,6 @@ void* get_page(Pager* pager, uint32_t page_num) {
 void *row_slot(Table *table, uint32_t row_num) {
     uint32_t page_num = row_num / ROW_PER_PAGE;
     void* page = get_page(table->pager, page_num);
-
     uint32_t row_offset = row_num % ROW_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
